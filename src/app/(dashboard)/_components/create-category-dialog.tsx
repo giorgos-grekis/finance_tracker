e0ecd1f8ee0f5a21c30b43fb60c0a358c6749fc1
cisu <georgrekis@gmail.com>
2024-06-24 14:00:26 +0300

    work with create transaction dialog

--- src/app/(dashboard)/_components/create-category-dialog.tsx
+++ src/app/(dashboard)/_components/create-category-dialog.tsx
@@ -1,57 +1,77 @@
-'use client';
-import type {  CreateCategorySchemaType } from "@/schema/categories";
+"use client";
+import type { CreateCategorySchemaType } from "@/schema/categories";
 import type { TransactionType } from "@/lib/types";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-import { CreateCategorySchema } from '@/schema/categories';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { CreateCategorySchema } from "@/schema/categories";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { PlusSquare } from "lucide-react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { cn } from "@/lib/utils";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
 type TypeCreateCategoryDialog = {
-    type: TransactionType
-}
+  type: TransactionType;
+};
 
+const CreateCategoryDialog = ({ type }: TypeCreateCategoryDialog) => {
+  const [open, setOpen] = useState(false);
+  const form = useForm<CreateCategorySchemaType>({
+    resolver: zodResolver(CreateCategorySchema),
+    defaultValues: {
+      type,
+    },
+  });
 
-const CreateCategoryDialog = ({type}: TypeCreateCategoryDialog) => {
-  
-    const [open, setOpen] = useState(false);
-    const form = useForm<CreateCategorySchemaType>({
-        resolver: zodResolver(CreateCategorySchema),
-        defaultValues: {
-            type,
-        }
-    })
-  
-    return (
-        <Dialog open={open} onOpenChange={setOpen}>
-            <DialogTrigger asChild>
-                <Button variant={"ghost"} className="flex border-separate items-center justify-start rounded-none border-b px-3 py-3 text-muted-foreground">
-                    <PlusSquare className="mr-2 h-4 w-4"/>
-                    Create new
-                </Button>
-            </DialogTrigger>    
-            <DialogContent>
-                <DialogTitle>
-                    Create <span className={cn('m-1', type === "income" ? "text-emerald-500": "text-rose-500")}>
-                        {type}
-                    </span>
-                    category 
-                </DialogTitle>   
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant={"ghost"}
+          className="flex border-separate items-center justify-start rounded-none border-b px-3 py-3 text-muted-foreground"
+        >
+          <PlusSquare className="mr-2 h-4 w-4" />
+          Create new
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogTitle>
+          Create{" "}
+          <span
+            className={cn(
+              "m-1",
+              type === "income" ? "text-emerald-500" : "text-rose-500"
+            )}
+          >
+            {type}
+          </span>
+          category
+        </DialogTitle>
 
-                <DialogDescription>
-                    Categories are used to group your transactions
-                </DialogDescription>             
+        <DialogDescription>
+          Categories are used to group your transactions
+        </DialogDescription>
 
-                <Form {...form}>
-                    <form className="space-y-8">
-                    <FormField
-                        control={form.control}
-                        name="name"
+        <Form {...form}>
+          <form className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Name</FormLabel>
@@ -64,12 +84,11 @@ const CreateCategoryDialog = ({type}: TypeCreateCategoryDialog) => {
                 </FormItem>
               )}
             />
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
-                    </form>
-                </Form>
-            </DialogContent>
-        </Dialog>
-      )
-}
-
-export default CreateCategoryDialog
+export default CreateCategoryDialog;
--- src/app/(dashboard)/_components/create-transaction-dialog.tsx
+++ src/app/(dashboard)/_components/create-transaction-dialog.tsx
@@ -1,21 +1,16 @@
 "use client";
-import type { TransactionType } from "@/lib/types";
-import type { ReactNode } from "react";
-import {
-  CreateTransactionSchema,
-  type CreateTransactionSchemaType,
-} from "@/schema/transaction";
 
-import { useState } from "react";
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
+  DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
-import { cn } from "@/lib/utils";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
@@ -25,102 +20,213 @@ import {
   FormLabel,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import CategoryPicker from "./category-picker";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { TransactionType } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import {
+  CreateCategorySchema,
+  CreateCategorySchemaType,
+} from "@/schema/categories";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CircleOff, Loader2, PlusSquare } from "lucide-react";
+import React, { ReactNode, useCallback, useState } from "react";
+import { useForm } from "react-hook-form";
+import Picker from "@emoji-mart/react";
+import data from "@emoji-mart/data";
+// import { useMutation, useQueryClient } from "@tanstack/react-query";
+// import { CreateCategory } from "@/app/(dashboard)/_actions/categories";
+import { Category } from "@prisma/client";
+import { toast } from "sonner";
+import { useTheme } from "next-themes";
 
 interface Props {
-  trigger: ReactNode;
   type: TransactionType;
+  successCallback: (category: Category) => void;
+  trigger?: ReactNode;
 }
 
-const CreateTransactionDialog = ({ trigger, type }: Props) => {
-  const form = useForm<CreateTransactionSchemaType>({
-    resolver: zodResolver(CreateTransactionSchema),
+function CreateCategoryDialog({ type, successCallback, trigger }: Props) {
+  const [open, setOpen] = useState(false);
+  const form = useForm<CreateCategorySchemaType>({
+    resolver: zodResolver(CreateCategorySchema),
     defaultValues: {
       type,
-      date: new Date(),
     },
   });
 
-  const [open, setOpen] = useState(false);
+  // const queryClient = useQueryClient();
+  const theme = useTheme();
+
+  // const { mutate, isPending } = useMutation({
+  //   mutationFn: CreateCategory,
+  //   onSuccess: async (data: Category) => {
+  //     form.reset({
+  //       name: "",
+  //       icon: "",
+  //       type,
+  //     });
+
+  //     toast.success(`Category ${data.name} created successfully 🎉`, {
+  //       id: "create-category",
+  //     });
+
+  //     successCallback(data);
+
+  //     await queryClient.invalidateQueries({
+  //       queryKey: ["categories"],
+  //     });
+
+  //     setOpen((prev) => !prev);
+  //   },
+  //   onError: () => {
+  //     toast.error("Something went wrong", {
+  //       id: "create-category",
+  //     });
+  //   },
+  // });
+
+  // const onSubmit = useCallback(
+  //   (values: CreateCategorySchemaType) => {
+  //     toast.loading("Creating category...", {
+  //       id: "create-category",
+  //     });
+  //     mutate(values);
+  //   },
+  //   [mutate]
+  // );
 
   return (
-    <Dialog open={true} onOpenChange={setOpen}>
-      <DialogTitle asChild>{trigger}</DialogTitle>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        {trigger ? (
+          trigger
+        ) : (
+          <Button
+            variant={"ghost"}
+            className="flex border-separate items-center justify-start roudned-none border-b px-3 py-3 text-muted-foreground"
+          >
+            <PlusSquare className="mr-2 h-4 w-4" />
+            Create new
+          </Button>
+        )}
+      </DialogTrigger>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>
-            Create a new{" "}
+            Create
             <span
               className={cn(
                 "m-1",
-                type === "income" ? "text-emerald-500" : "text-rose-500"
+                type === "income" ? "text-emerald-500" : "text-red-500"
               )}
             >
               {type}
             </span>
-            transaction
+            category
           </DialogTitle>
+          <DialogDescription>
+            Categories are used to group your transactions
+          </DialogDescription>
         </DialogHeader>
         <Form {...form}>
-          <form className="space-y-4">
-            {/* Description */}
+          <form
+          //  onSubmit={form.handleSubmit(onSubmit)} 
+           className="space-y-8">
             <FormField
               control={form.control}
-              name="description"
+              name="name"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Description</FormLabel>
+                  <FormLabel>Name</FormLabel>
                   <FormControl>
-                    <Input defaultValue={""} {...field} />
+                    <Input placeholder="Category" {...field} />
                   </FormControl>
                   <FormDescription>
-                    Transaction description (optional)
+                    This is how your category will appear in the app
                   </FormDescription>
                 </FormItem>
               )}
             />
 
-            {/* Amount */}
+            {/* Icon */}
             <FormField
               control={form.control}
-              name="amount"
+              name="icon"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Amount</FormLabel>
+                  <FormLabel>Icon</FormLabel>
                   <FormControl>
-                    <Input defaultValue={0} type="number" {...field} />
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant={"outline"}
+                          className="h-[100px] w-full"
+                        >
+                          {form.watch("icon") ? (
+                            <div className="flex flex-col items-center gap-2">
+                              <span className="text-5xl" role="img">
+                                {field.value}
+                              </span>
+                              <p className="text-xs text-muted-foreground">
+                                Click to change
+                              </p>
+                            </div>
+                          ) : (
+                            <div className="flex flex-col items-center gap-2">
+                              <CircleOff className="h-[48px] w-[48px]" />
+                              <p className="text-xs text-muted-foreground">
+                                Click to select
+                              </p>
+                            </div>
+                          )}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-full">
+                        <Picker
+                          data={data}
+                          theme={theme.resolvedTheme}
+                          onEmojiSelect={(emoji: { native: string }) => {
+                            field.onChange(emoji.native);
+                          }}
+                        />
+                      </PopoverContent>
+                    </Popover>
                   </FormControl>
                   <FormDescription>
-                    Transaction amount (required)
+                    This is how your category will appear in the app
                   </FormDescription>
                 </FormItem>
               )}
             />
-
-            {/* category field */}
-            <div className="flex items-center justify-between gap-2">
-            <FormField
-              control={form.control}
-              name="category"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Category</FormLabel>
-                  <FormControl>
-                    <CategoryPicker type={type}/>
-                    {/* <Input defaultValue={0} type="number" {...field} /> */}
-                  </FormControl>
-                  <FormDescription>
-                    Select a category for this transaction
-                  </FormDescription>
-                </FormItem>
-              )}
-            />
-            </div>
           </form>
         </Form>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button
+              type="button"
+              variant={"secondary"}
+              onClick={() => {
+                form.reset();
+              }}
+            >
+              Cancel
+            </Button>
+          </DialogClose>
+          <Button 
+          // onClick={form.handleSubmit(onSubmit)} disabled={isPending}
+          >
+            save
+            {/* {!isPending && "Create"} */}
+            {/* {isPending && <Loader2 className="animate-spin" />} */}
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
-};
+}
 
-export default CreateTransactionDialog;
+export default CreateCategoryDialog;
